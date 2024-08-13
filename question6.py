58aea08d10bd6fbc75bc90eb80f4b8dd83d457fe
hangy6 <hangy6@illinois.edu>
2024-08-13 13:42:36 -0500

    fit svi model

--- question6.py
+++ question6.py
@@ -7,6 +7,9 @@ from utils import Binomial, European_option_price
 import os
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.patches as mpatches
+import torch
+import torch.nn as nn
 
 def deAmericanize(option, K, T, S, target_price, r, q, sigma_low, sigma_high, epsilon):
     count = 0
@@ -15,17 +18,33 @@ def deAmericanize(option, K, T, S, target_price, r, q, sigma_low, sigma_high, ep
         sigma_mid = (sigma_low + sigma_high) / 2
         curr_price = European_option_price(option, S, r, q, K, T, sigma_mid)
         # print(f'Current sigma = {sigma_mid}, price diff = {abs(curr_price - target_price)}')
-        count += 1
-        if count > 1000:
-            # the search interval length would be of sigma_high / 2**20, very small
-            # meaning that the sigma is likely to be out of boundary, binary search fails
-            return -1
+#         count += 1
+#         if count > 1000:
+#             # the search interval length would be of sigma_high / 2**20, very small
+#             # meaning that the sigma is likely to be out of boundary, binary search fails
+#             return -1
         if curr_price < target_price - epsilon:
             sigma_low = sigma_mid
         elif curr_price > target_price + epsilon:
             sigma_high = sigma_mid
         else:
             return sigma_mid
+
+class SVI_Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.a = nn.Parameter(torch.tensor(0.))
+        self.b = nn.Parameter(torch.tensor(0.))
+        self.rho = nn.Parameter(torch.tensor(0.))
+        self.m = nn.Parameter(torch.tensor(0.))
+        self.sigma = nn.Parameter(torch.tensor(0.))
+    
+    def forward(self, k):
+        var = self.a + self.b * (self.rho * (k - self.m) + torch.sqrt((k - self.m)**2 + self.sigma**2))
+        return var
+
+def get_SVI_param_from_model(model):
+    return np.round(np.array([model.a.item(), model.b.item(), model.rho.item(), model.m.item(), model.sigma.item()]), 4)
 
 if __name__ == '__main__':
     
@@ -37,16 +56,22 @@ if __name__ == '__main__':
         data = pd.read_csv(results_path)
     else:
         data = pd.read_csv('ProjectOptionData.csv')
-    
+        
+        print(f'Original data size: {len(data)}')
         # prune untraded S and anomaly
         data[f' [{option}_LAST]'] = data[f' [{option}_LAST]'].str.strip()
         data[f' [{option}_LAST]'].replace('', '0', inplace=True)
-        data = data[data[f' [{option}_LAST]'] != '0'].reset_index(drop=True)
-
-        S = data[f' [UNDERLYING_LAST]'] # 219.19
-        K = data[f' [STRIKE]']
-        T = data[f' [DTE]'] / 365
-        option_price = data[f' [{option}_ASK]'].astype(float)
+        data = data[data[f' [{option}_LAST]'] != '0']
+        # we only use TTM <= 1 year data and K/S moneyness between 0.5 - 1.5
+        data = data[(data[' [DTE]'] <= 365) & (data[' [STRIKE]'] / data[' [UNDERLYING_LAST]'] <= 1.5) & (data[' [STRIKE]'] / data[' [UNDERLYING_LAST]'] >= 0.5)]
+        data = data.reset_index(drop=True)
+        print(f'Fileter data size: {len(data)}')
+        
+        S = data[' [UNDERLYING_LAST]'] # 219.19
+        K = data[' [STRIKE]']
+        data['TTM'] = T = data[' [DTE]'] / 365
+        option_price = (data[f' [{option}_ASK]'].astype(float) + data[f' [{option}_BID]'].astype(float)) / 2 # use the middle price
+        data[f'{option}_PRICE'] = option_price
         r = 0.05
         q = 0
         #     sigma = 0.2
@@ -64,18 +89,58 @@ if __name__ == '__main__':
             calibrated_sigma = deAmericanize(option, K[i], T[i], float(S[i]), option_price[i], r, q, sigma_low, sigma_high, epsilon)
             calibrated_sigmas.append(calibrated_sigma)
 #             tqdm.write(f'K = {K[i]}, T = {T[i]}, S = {S[i]}, ask = {option_price[i]}, calibrated sigma = {calibrated_sigma}')
-        data['sigma'] = calibrated_sigmas
-        data = data[data['sigma'] != -1]
-        data = data[[f' [UNDERLYING_LAST]', ' [STRIKE]', ' [DTE]', f' [{option}_ASK]', 'sigma']]
+        data['SIGMA'] = calibrated_sigmas
+        data = data[data['SIGMA'] != -1]
+        data['K/S'] = data[' [STRIKE]'] / data[' [UNDERLYING_LAST]'] # K/S moneyness
+        data = data[[f' [UNDERLYING_LAST]', 'K/S', 'TTM', f'{option}_PRICE', 'SIGMA']]
         data.to_csv(results_path)
-        
     
+    # fit svi model
+    svi_results_path = f'project_{option}_svi.csv'
+    if os.path.exists(svi_results_path):
+        data = pd.read_csv(svi_results_path)
+    else:
+        EPOCHS = 200
+        criterion = nn.L1Loss()
+        grouped = data.groupby('TTM')
+        for group_name, group_df in grouped:
+            print(f"Fitting model when TTM = {group_name}")
+            model = SVI_Model()
+            optimizer = torch.optim.Adam(model.parameters(), lr=0.01) # using SGD for optimization
+            model.train()
+            k = torch.log(torch.from_numpy(group_df['K/S'].values)) # log-moneyness
+            var = torch.from_numpy(group_df['SIGMA'].values)**2 # variance is sigma**2
+        
+            loss = torch.tensor(float('inf'))
+            for epoch in range(EPOCHS):
+                pred_var = model(k)
+                loss = criterion(pred_var, var)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                if (epoch + 1) % 10 == 0:
+                    print(f'In Epoch [{epoch + 1} / {EPOCHS}], loss (mean difference between model predicted variance and market variance) = {loss.item()}')
+            model.eval()
+            data.loc[group_df.index, 'PRED_SIGMA'] = model(k).detach().numpy()
+            print(f'At TTM = {group_name}, (a, b, m, ρ, σ) = ({get_SVI_param_from_model(model)})')
+            print("-" * 40)
+        data.to_csv(svi_results_path)
+            
+    # plot the implied volatility surface
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.plot_trisurf(data[' [STRIKE]'], data[' [DTE]'], data['sigma'], cmap='viridis')
-    ax.set_xlabel('Strike Price (K)')
-    ax.set_ylabel('Time to Maturity (T day)')
+    market_surf = ax.plot_trisurf(data['K/S'], data['TTM'], data['SIGMA'], cmap='viridis', alpha=0.2, label='market surface')
+    svi_surf = ax.plot_trisurf(data['K/S'], data['TTM'], data['PRED_SIGMA'], cmap='plasma', alpha=0.5, label='SVI surface')
+    ax.set_xlabel('K/S Moneyness')
+    ax.set_ylabel('Time to Maturity (Year)')
     ax.set_zlabel('Implied Volatility (Sigma)')
-    ax.set_title('Implied Volatility Surface')
+    ax.set_zlim(0,2)
+    option_name = {'C':'Call', 'P':'Put'}
+    ax.set_title(f'{option_name[option]} Option Implied Volatility Surface')
+    
+    fig.colorbar(market_surf, label='market surface')
+    fig.colorbar(svi_surf, pad=0.2, label='SVI surface')
+    fig.tight_layout()
     plt.savefig('question6.png')
+    
 
